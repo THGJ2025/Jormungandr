3bb1658d3023e15225b4489823f40e31d4835d02
Arek <arek0006@gmail.com>
2025-06-05 00:58:50 +0200

    Ritual bell recalls after max distance

--- Source/Jormungandr/InventoryItem/JRitualBell.cpp
+++ Source/Jormungandr/InventoryItem/JRitualBell.cpp
@@ -2,6 +2,8 @@
 
 
 #include "JRitualBell.h"
+
+#include "GameFramework/Character.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "GameFramework/RotatingMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
@@ -13,8 +15,7 @@ AJRitualBell::AJRitualBell()
 
 	BellMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bell Mesh"));
 	SetRootComponent(BellMesh);
-
-	BellMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	BellMesh->SetCollisionProfileName(FName("NoCollision"));
 	BellMesh->SetGenerateOverlapEvents(false);
 
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
@@ -24,13 +25,39 @@ AJRitualBell::AJRitualBell()
 
 	RotatingMovementComponent = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("Rotating Movement Component"));
 	RotatingMovementComponent->SetUpdatedComponent(GetRootComponent());
-	RotatingMovementComponent->RotationRate = FRotator(-1440.f, 0.f, 0.f);
 	RotatingMovementComponent->bAutoActivate = false;
+
+	BellMesh->OnComponentHit.AddDynamic(this, &AJRitualBell::OnMeshHit);
+}
+
+void AJRitualBell::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OnFlightCompletedDelegate.BindUObject(this, &AJRitualBell::OnBellFlightCompleted);
 }
 
 void AJRitualBell::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	switch (BellState)
+	{
+	case EBellState::EBS_None:
+		break;
+	case EBellState::EBS_Idle:
+		break;
+	case EBellState::EBS_IdleAfterImpact:
+		break;
+	case EBellState::EBS_PostLaunch:
+		PostLaunchTick();
+		break;
+	case EBellState::EBS_FlyingToTarget:
+		FlyToTargetTick();
+		break;
+	case EBellState::EBS_Max:
+		break;
+	}
 }
 
 void AJRitualBell::PrimaryUse()
@@ -46,18 +73,114 @@ void AJRitualBell::PrimaryUse()
 
 	FVector ViewportCenterPosition{};
 	FVector ViewportCenterDirection{};
-	UGameplayStatics::DeprojectScreenToWorld(Cast<APlayerController>(GetInstigator()->GetController()), ViewportCenter, ViewportCenterPosition, ViewportCenterDirection);
+	UGameplayStatics::DeprojectScreenToWorld(Cast<APlayerController>(GetInstigator()->GetController()),
+	                                         ViewportCenter,
+	                                         ViewportCenterPosition,
+	                                         ViewportCenterDirection);
 
+	InitialLocation = ViewportCenterPosition;
 	SetActorLocation(ViewportCenterPosition);
+
+	RotatingMovementComponent->RotationRate = BellThrowRotationRate;
 
 	Launch(ViewportCenterDirection * FlightSpeed);
 }
 
+void AJRitualBell::OnMeshHit(UPrimitiveComponent* HitComponent,
+                             AActor* OtherActor,
+                             UPrimitiveComponent* OtherComp,
+                             FVector NormalImpulse,
+                             const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Bell hit: %s"), *OtherActor->GetName())
+	ProjectileMovementComponent->Deactivate();
+	RotatingMovementComponent->Deactivate();
+}
+
 void AJRitualBell::Launch(const FVector& Velocity)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Launch Velocity: %s"), *Velocity.ToString())
+	BellMesh->SetCollisionProfileName(FName("IgnoreOnlyPawn"));
 	ProjectileMovementComponent->Velocity = Velocity;
 	ProjectileMovementComponent->Activate();
 
 	RotatingMovementComponent->Activate();
+
+	BellState = EBellState::EBS_PostLaunch;
+}
+
+void AJRitualBell::RecallAxe()
+{
+	const FVector PlayerRightHandLocation{Cast<ACharacter>(GetInstigator())->GetMesh()->GetSocketLocation(FName("hand_r_Socket"))};
+
+	FlyToTarget(GetInstigator()->GetActorQuat(), PlayerRightHandLocation, BellRecallMinSpeed, BellRecallMaxDuration);
+}
+
+void AJRitualBell::FlyToTarget(const FQuat& TargetRotation, const FVector& TargetLocation, float MinSpeed, float FlightMaxDuration)
+{
+	BellState = EBellState::EBS_FlyingToTarget;
+
+	InitialLocation = GetActorLocation();
+
+	FlightInfo.TargetLocation = TargetLocation;
+	FlightInfo.TargetRotation = TargetRotation;
+
+	const double DistanceToTarget{FVector::Dist(FlightInfo.TargetLocation, InitialLocation)};
+
+	/**
+	 * If the time calculated based on MinSpeed is less than FlightMaxDuration, it will be the chosen duration of the flight.
+	 */
+	FlightInfo.Duration = FMath::Min(DistanceToTarget / MinSpeed, FlightMaxDuration);
+
+	SetActorRotation(FlightInfo.TargetRotation);
+
+	GetWorldTimerManager().SetTimer(FlightInfo.FlightTimer, this, &AJRitualBell::OnFlightTimerCompleted, FlightInfo.Duration, false);
+}
+
+void AJRitualBell::PostLaunchTick()
+{
+	if (FVector::Dist(InitialLocation, GetActorLocation()) >= MaxDistance)
+	{
+		ProjectileMovementComponent->Deactivate();
+		RotatingMovementComponent->Deactivate();
+
+		RecallAxe();
+	}
+}
+
+void AJRitualBell::FlyToTargetTick()
+{
+	InterpToTargetLocation();
+}
+
+void AJRitualBell::InterpToTargetLocation()
+{
+	const float TimeElapsed{GetWorldTimerManager().GetTimerElapsed(FlightInfo.FlightTimer)};
+
+	/**
+	 * The percentage of how close it is to the tagret location.
+	 */
+	const double Alpha{TimeElapsed / FlightInfo.Duration};
+
+	const FVector NewLocation{FMath::Lerp(InitialLocation, FlightInfo.TargetLocation, Alpha)};
+	UE_LOG(LogTemp, Warning, TEXT("NewLocation: %s"), *NewLocation.ToString())
+	SetActorLocation(NewLocation);
+}
+
+void AJRitualBell::OnFlightTimerCompleted()
+{
+	BellMesh->SetCollisionProfileName(FName("NoCollision"));
+
+	SetActorRotation(FlightInfo.TargetRotation);
+	SetActorLocation(FlightInfo.TargetLocation);
+
+	OnFlightCompletedDelegate.ExecuteIfBound();
+
+	BellState = EBellState::EBS_Idle;
+}
+
+void AJRitualBell::OnBellFlightCompleted()
+{
+	AttachToComponent(Cast<ACharacter>(GetInstigator())->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("hand_r_Socket"));
+
+	OnBellRecallCompletedDelegate.ExecuteIfBound();
 }
--- Source/Jormungandr/InventoryItem/JRitualBell.h
+++ Source/Jormungandr/InventoryItem/JRitualBell.h
@@ -9,6 +9,9 @@
 class URotatingMovementComponent;
 class UProjectileMovementComponent;
 
+DECLARE_DELEGATE(FOnFlightCompletedSignature)
+DECLARE_DELEGATE(FOnBellRecallCompletedSignature)
+
 UCLASS()
 class JORMUNGANDR_API AJRitualBell : public AJInventoryItem
 {
@@ -17,17 +20,124 @@ class JORMUNGANDR_API AJRitualBell : public AJInventoryItem
 public:
 	AJRitualBell();
 
+protected:
+	virtual void BeginPlay() override;
+
+public:
+
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void PrimaryUse() override;
 
 #pragma region ThrowAndRecall
 
+public:
+	UFUNCTION()
+	void OnMeshHit(UPrimitiveComponent* HitComponent,
+	               AActor* OtherActor,
+	               UPrimitiveComponent* OtherComp,
+	               FVector NormalImpulse,
+	               const FHitResult& Hit);
+	/**
+	 * Delegate used to notify the subscriber when it has arrived at its target.
+	 */
+	FOnFlightCompletedSignature OnFlightCompletedDelegate;
+
+	FOnBellRecallCompletedSignature OnBellRecallCompletedDelegate;
+
 private:
 	void Launch(const FVector& Velocity);
 
+	void RecallAxe();
+
+	/**
+	 * Initiates the Bell to fly to a target. Mostly recalling to the right hand, but can also have a different target
+	 */
+	UFUNCTION()
+	void FlyToTarget(const FQuat& TargetRotation, const FVector& TargetLocation, float MinSpeed, float FlightMaxDuration);
+
+	/**
+	 * Function called in Tick if the Bell is in the PostLaunch state.
+	 */
+	void PostLaunchTick();
+
+	void FlyToTargetTick();
+
+	/**
+	 * Interpolates the Bell's location to its target location.
+	 */
+	void InterpToTargetLocation();
+
+	/**
+	 * Called once the FlightInfo.FlightTimer has completed.
+	 * Changes its state and notifies all subscribers of the OnFlightCompleted delegate.
+	 */
+	void OnFlightTimerCompleted();
+
+	/**
+	 * Called when the Bell has completed its flight to its target.
+	 * In this case, it's when the Bell recall is complete.
+	 * Before the function is complete, it notifies it's subscriber (the player) that the recall is complete.
+	 */
+	void OnBellFlightCompleted();
+
+	enum class EBellState : uint8
+	{
+		EBS_None,
+		/**
+		 * State when the axe is idle. Used when a player is holding the Ritual Bell.
+		 */
+		EBS_Idle,
+		/**
+		 * State when Bell has hit something after being launched by the projectile movement component.
+		 */
+		EBS_IdleAfterImpact,
+		/**
+		 * State immediately after it has been launched by the projectile movement component.
+		 */
+		EBS_PostLaunch,
+		/**
+		 * State when it is flying to a specific target and is flying without assistance from the projectile movement component.
+		 */
+		EBS_FlyingToTarget,
+		EBS_Max
+	};
+	/**
+	 * Used to encapsulate all the data responsible for flying the Bell to its target.
+	 */
+	struct FFlightInfo
+	{
+		FQuat InitialRotation{};
+		FQuat TargetRotation{};
+		FVector TargetLocation{};
+		/**
+		 * Timer used to calculate the alpha value between the initial location and the target location.
+		 */
+		FTimerHandle FlightTimer{};
+		float Duration{};
+		float InterpToTargetRotationDuration{};
+	};
+
+	FFlightInfo FlightInfo{};
+
+	EBellState BellState{EBellState::EBS_Idle};
+
 	UPROPERTY(EditAnywhere, Category="J|Properties")
 	float FlightSpeed{500.f};
+
+	UPROPERTY(EditAnywhere, Category="J|Properties")
+	float MaxDistance{200.f};
+
+	UPROPERTY(EditAnywhere, Category="J|Properties")
+	FRotator BellThrowRotationRate{-1440.f, 0.f, 0.f};
+
+	UPROPERTY(EditAnywhere, Category="J|Properties")
+	float BellRecallMinSpeed{1600.f};
+
+	UPROPERTY(EditAnywhere, Category="J|Properties")
+	float BellRecallMaxDuration{1.f};
+
+	FVector InitialLocation{};
 
 	UPROPERTY(VisibleAnywhere, Category="J|Mesh")
 	TObjectPtr<UStaticMeshComponent> BellMesh;
